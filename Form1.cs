ecc7d2bb51fe08b24a0ee65250bbe741388e7c63
Zequn Guan <g760420431@outlook.com>
2020-11-30 10:50:23 +0800

    update

--- Form1.cs
+++ Form1.cs
@@ -6,13 +6,28 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace 票务管理系统
 {
     public partial class Form1 : Form
     {
+        private static string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\database.mdf;Integrated Security=True";
+        public static SqlConnection db = new SqlConnection(connString);
         public Form1()
         {
+            try
+            {
+                db.Open();
+                MessageBox.Show("数据库连接成功");
+                SqlCommand command = new SqlCommand("select * from users", db);
+                MessageBox.Show(command.ExecuteScalar().ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("数据库连接失败！" + ex.Message);
+            }
+
             InitializeComponent();
         }
 
