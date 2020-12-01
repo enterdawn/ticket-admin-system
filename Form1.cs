57147a7daeb1f0d44155d73a22c2ed8d5aa50ccc
Zequn Guan <g760420431@outlook.com>
2020-12-01 19:51:57 +0800

    up

--- Form1.cs
+++ Form1.cs
@@ -11,24 +11,6 @@ using System.Security.Cryptography;
 
 namespace 票务管理系统
 {
-    public class md532
-    {
-        public static string getpass(string pass)
-        {
-            string cl = pass;
-            string pwd = "";
-            MD5 md5 = MD5.Create(); //实例化一个md5对像
-                                    // 加密后是一个字节类型的数组，这里要注意编码UTF8/Unicode等的选择　
-            byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(cl));
-            // 通过使用循环，将字节类型的数组转换为字符串，此字符串是常规字符格式化所得
-            for (int i = 0; i < s.Length; i++)
-            {
-                // 将得到的字符串使用十六进制类型格式。格式后的字符是小写的字母，如果使用大写（X）则格式后的字符是大写字符 
-                pwd = pwd + s[i].ToString("X");
-            }
-            return pwd;
-        }
-    }
     
     public partial class Form1 : Form
     {
@@ -68,6 +50,24 @@ namespace 票务管理系统
 
         }
 
-        
+        public class md532
+        {
+            public static string getpass(string pass)
+            {
+                string cl = pass;
+                string pwd = "";
+                MD5 md5 = MD5.Create(); //实例化一个md5对像
+                                        // 加密后是一个字节类型的数组，这里要注意编码UTF8/Unicode等的选择　
+                byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(cl));
+                // 通过使用循环，将字节类型的数组转换为字符串，此字符串是常规字符格式化所得
+                for (int i = 0; i < s.Length; i++)
+                {
+                    // 将得到的字符串使用十六进制类型格式。格式后的字符是小写的字母，如果使用大写（X）则格式后的字符是大写字符 
+                    pwd = pwd + s[i].ToString("X");
+                }
+                return pwd;
+            }
+        }
+
     }
 }
