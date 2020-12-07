5071a9a6bf9b9d25946c8cd465d4fc651147e9ba
Zequn Guan <g760420431@outlook.com>
2020-12-07 16:38:18 +0800

    123

--- users.cs
+++ users.cs
@@ -53,18 +53,35 @@ namespace 票务管理系统
             try
             {
                 db.Open();
-               // SqlCommand cmd = new SqlCommand("select nickname,IDCardNumber,_admin,_status,regTime,active,sex,phoneNum,email from _user where _admin = '0';", db);
-              //  SqlDataAdapter da = new SqlDataAdapter();
-              //  da.SelectCommand = cmd;
-              //  DataSet ds = new DataSet();
-              //  da.Fill(ds);
-              //  //MessageBox.Show(ds.Tables[0].Rows)
-              //  db.Close();
+                SqlCommand cmd0 = new SqlCommand();
+                cmd0.CommandText = "select userid from _user where nickname=@nickmane;";
+                SqlParameter[] paras =
+                {
+                    new SqlParameter("@nickmane",name),
+                };
+                cmd0.Parameters.AddRange(paras);
+                SqlDataAdapter nickN = new SqlDataAdapter();//实例化sqldataadpter
+                nickN.SelectCommand = cmd0;//设置为已实例化SqlDataAdapter的查询命令
+                DataSet nick = new DataSet();//实例化dataset
+                nickN.Fill(nick);//把数据填充到dataset
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText="select ticketID,butTime,price,seat,insertTime,varifyOrNot,flightNumber,refundOrnot from _user where nickname = @UID  and deleteOrNot=0;";
+                SqlParameter[] paras2 =
+                {
+                    new SqlParameter("@UID",nick.Tables[0]),
+                };
+                cmd.Parameters.AddRange(paras2);
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                //MessageBox.Show(ds.Tables[0].Rows)
+                db.Close();
             }
             catch (Exception ex)
             {
-              //  MessageBox.Show("未知错误\r\n" + ex.Message);
-              //  Dispose();
+                MessageBox.Show("未知错误\r\n" + ex.Message);
+                Dispose();
             }
         }
     }
