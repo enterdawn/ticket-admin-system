a68f689548406749e60ba4781872cae99b26c2a1
abovhan <1330171291@qq.com>
2020-12-07 11:32:30 +0800

    fbv

--- modifyUserInfo.Designer.cs
+++ modifyUserInfo.Designer.cs
@@ -37,33 +37,34 @@ namespace 票务管理系统
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(24, 21);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(48, 42);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 37;
-            this.dataGridView1.Size = new System.Drawing.Size(979, 372);
+            this.dataGridView1.Size = new System.Drawing.Size(1958, 744);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(446, 409);
-            this.btn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn1.Location = new System.Drawing.Point(892, 818);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(110, 44);
+            this.btn1.Size = new System.Drawing.Size(220, 88);
             this.btn1.TabIndex = 1;
             this.btn1.Text = "提交";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // modifyUserInfo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1026, 520);
+            this.ClientSize = new System.Drawing.Size(2052, 1040);
             this.Controls.Add(this.btn1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "modifyUserInfo";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.modifyUserInfo_Load);
--- modifyUserInfo.cs
+++ modifyUserInfo.cs
@@ -16,12 +16,30 @@ namespace 票务管理系统
         private string Pwd;
         private static string connString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =\"" + Environment.CurrentDirectory + "\\database.mdf\"; Integrated Security = False; Connect Timeout = 30";
         private SqlConnection db = new SqlConnection(connString);
+        private List<List<string>> oldData = new List<List<string>>();
+        private List<List<string>> newData = new List<List<string>>();
+        private List<string> itemName = new List<string>();
         public modifyUserInfo(string name,string pwd)
         {
             InitializeComponent();
             Name = name;
             Pwd = pwd;
             init();
+        }
+
+        private void getFormData(List<List<string>> data)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            foreach (var row in dataGridView1.Rows)
+            {
+                List<string> temp = new List<string>();
+                
+                foreach(DataGridViewColumn col in dataGridView1.Columns)
+                {
+                    temp.Add(row.Cells[col.Name].Value.ToString());
+                }
+                data.Add(temp);
+            }
         }
         private void init()
         {
@@ -34,6 +52,14 @@ namespace 票务管理系统
                 DataSet ds = new DataSet();
                 da.Fill(ds);
                 dataGridView1.DataSource = ds.Tables[0];
+                getFormData(oldData);
+                for(int i = 0; i < oldData.Count; i++)
+                {
+                    for(int j = 0; j < oldData[i].Count; j++)
+                    {
+                        MessageBox.Show(oldData[i][j]);
+                    }
+                }
                 db.Close();
             }
             catch(Exception ex)
@@ -41,6 +67,15 @@ namespace 票务管理系统
                 MessageBox.Show("未知错误\r\n"+ex.Message);
                 Dispose();
             }
+            itemName.Add("nickname");
+            itemName.Add("IDCardNumber");
+            itemName.Add("_admin");
+            itemName.Add("_status");
+            itemName.Add("regTime");
+            itemName.Add("active");
+            itemName.Add("sex");
+            itemName.Add("phoneNum");
+            itemName.Add("email");
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -59,7 +94,54 @@ namespace 票务管理系统
 
         private void modifyUserInfo_Load(object sender, EventArgs e)
         {
+            
+        }
 
+
+
+
+        private void update(string itemName,string val,string nickname)
+        {
+            try
+            {
+                db.Open();
+                SqlCommand cmd = new SqlCommand("update _user set " + itemName + " = '" + val + "' where nickname = '" + nickname + "';", db);
+                cmd.ExecuteNonQuery();
+                db.Close();
+                MessageBox.Show("修改成功");
+                this.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("更新失败\r\n", ex.Message);
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            getFormData(newData);
+            MessageBox.Show(newData.Count.ToString());
+            int rowCnt = newData.Count;
+            if (rowCnt == 0)
+            {
+                this.Close();
+                return;
+            }
+            int colCnt = newData[0].Count;
+            MessageBox.Show("modify");
+            for (int i = 0; i < rowCnt; i++)
+            {
+                for(int j = 0; j < colCnt; j++)
+                {
+                    MessageBox.Show(oldData[i][j].ToString(), newData[i][j].ToString());
+                    if (oldData[i][j] != newData[i][j])
+                    {
+                        
+                        update(itemName[j], newData[i][j],newData[i][0]);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
